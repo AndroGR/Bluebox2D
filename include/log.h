fc396292ce83b2ca9af4ab3f900cf504539e2e97
Aggelos Tselios <androtechgr@gmail.com>
2022-06-18 10:24:28 +0300

    log.h: Declare LogMessage()

--- include/log.h
+++ include/log.h
@@ -1,6 +1,11 @@
 #ifndef _LOG_H
 #define _LOG_H
 #include "Bluebox.h"
+// Logs an SDL error to the console
 void LogToBluebox(int errlevel, const char* msg);
+
+// Gets the latest errno, gets the string describing it, and then prints it to
+// the console, along with the message provided here.
+void LogMessage(const char* message);
 
 #endif /* _LOG_H */
